95a65ac6b97abb8fe7594309fbb5b289e8277785
ent3r <32072697+ent3r@users.noreply.github.com>
2020-10-14 22:09:00 +0200

    :construction: Add mongoose connection

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,9 +1,13 @@
 import { Application } from "express";
 
 import expressSetup from "./express";
+import mongooseSetup from "./mongoose";
 import Logger from "./logger";
 
 export default async (app: Application): Promise<void> => {
   expressSetup(app);
   Logger.info("Express initialized");
+
+  await mongooseSetup();
+  Logger.info("Mongoose initialized");
 };
---
+++ src/loaders/mongoose.ts
@@ -0,0 +1,29 @@
+import mongoose = require("mongoose");
+
+import config from "../config";
+import Logger from "./logger";
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+
+export default async (): Promise<void> => {
+  mongoose.connection.on("error", (err) => {
+    Logger.error(err);
+  });
+  mongoose.connection.once("connected", () => {
+    Logger.info("MongoDB connected");
+  });
+  mongoose.connection.on("disconnected", () => {
+    Logger.warn("MongoDB disconnected");
+  });
+  mongoose.connection.on("reconnected", () => {
+    Logger.info("MongoDB reconnected after disconnecting");
+  });
+  mongoose.connection.on("reconnectFailed", () => {
+    Logger.error("MongoDB reconnect failed");
+  });
+  Logger.debug("Mongoose events registered");
+  await mongoose.connect(`${config.get("db.host")}/${config.get("db.db")}`);
+};
