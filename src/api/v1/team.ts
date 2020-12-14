bd552beea4231b51c7737d0c1ad58624e7e6c54f
theS1LV3R <32072697+theS1LV3R@users.noreply.github.com>
2020-12-14 20:46:56 +0100

    feat: ability to get team details

--- src/api/v1/team.ts
+++ src/api/v1/team.ts
@@ -6,12 +6,25 @@ import { UnauthorizedError } from "../../types/httperrors";
 
 const verifyTeamCreation = celebrate({
   [Segments.HEADERS]: Joi.object({
-    authorization: Joi.string().regex(
-      /^Bearer [A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.[A-Za-z0-9-_.+/=]+$/
-    ).optional(),
+    authorization: Joi.string()
+      .regex(/^Bearer [A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.[A-Za-z0-9-_.+/=]+$/)
+      .optional(),
   }).unknown(true),
   [Segments.BODY]: Joi.object({
     teamName: Joi.string().required().min(3).max(64),
+  }),
+});
+
+const verifyGetTeam = celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string()
+      .regex(/^Bearer [A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.[A-Za-z0-9-_.+/=]+$/)
+      .optional(),
+  }).unknown(true),
+  [Segments.PARAMS]: Joi.object({
+    teamID: Joi.string()
+      .regex(/^[a-f\d]{24}$/i)
+      .required(),
   }),
 });
 
@@ -19,6 +32,7 @@ export default (): Router => {
   const router = Router();
 
   router.post("/", verifyTeamCreation, createTeam);
+  router.get("/:teamID", verifyGetTeam, getTeam);
 
   return router;
 };
@@ -33,5 +47,16 @@ function createTeam(req: Request, res: Response, next: NextFunction) {
   teamService
     .createTeam(req.headers.authorization.slice(7), req.body.teamName)
     .then((teamData) => res.status(201).send(teamData))
+    .catch((err) => next(err));
+}
+
+function getTeam(req: Request, res: Response, next: NextFunction) {
+  if (!req.headers.authorization) {
+    next(new UnauthorizedError({ message: "Missing authorization" }));
+  }
+
+  teamService
+    .getTeam(req.headers.authorization.slice(7), req.params.teamID)
+    .then((teamDetails) => res.send(teamDetails))
     .catch((err) => next(err));
 }
--- src/services/Team.ts
+++ src/services/Team.ts
@@ -1,11 +1,12 @@
 import jsonWebToken from "jsonwebtoken";
 
-import { TeamModel } from "../models/Team";
+import { TeamModel, ITeamModel } from "../models/Team";
 import { UserModel } from "../models/User";
 import {
   BadRequestError,
   ConflictError,
   InternalServerError,
+  UnauthorizedError,
 } from "../types/httperrors";
 import { JWTData } from "../types";
 import config from "../config";
@@ -21,7 +22,9 @@ export default class TeamService {
   ): Promise<{ teamName: string; teamID: string }> {
     // TODO: Turn this into a proper type
 
-    const teamExists = await TeamModel.exists({ name: teamName.toLowerCase() }).then();
+    const teamExists = await TeamModel.exists({
+      name: teamName.toLowerCase(),
+    }).then();
 
     if (teamExists) throw new ConflictError({ errorCode: "error_team_exists" });
 
@@ -52,5 +55,42 @@ export default class TeamService {
     owner.save();
 
     return { teamName: team.name, teamID: team._id };
+  }
+
+  /**
+   * Get's a team's information. By default only allows to get info if the user is in the team, but admins can always get the team details
+   *
+   * @async
+   * @param {string} jwt the JWT of the user trying to get team details
+   * @param {string} teamID the ID of the team that is getting fetched
+   * @memberof TeamService
+   */
+  public async getTeam(jwt: string, teamID: string): Promise<ITeamModel> {
+    /* eslint-disable-next-line */
+    let decodedJWT: string | object;
+    try {
+      decodedJWT = jsonWebToken.verify(jwt, config.get("jwt.secret"));
+    } catch {
+      throw new BadRequestError({ message: "Invalid JWT" });
+    }
+
+    const user = await (await UserModel.findById((decodedJWT as JWTData).id))
+      .execPopulate()
+      .then()
+      .catch((err) => {
+        Logger.error(err);
+        throw new InternalServerError();
+      });
+
+    let team: ITeamModel;
+    if (teamID in user.teams) {
+      team = await TeamModel.findById(teamID);
+    } else if ((decodedJWT as JWTData).isAdmin) {
+      team = await TeamModel.findById(teamID);
+    } else {
+      throw new UnauthorizedError();
+    }
+
+    return team;
   }
 }
