e07b874f010914f426ef03168e266cf6bd99f1e9
S1LV3R <s1lv3r@corax.team>
2021-01-24 21:03:27 +0100

    feat: createCTF method in CTFService class

--- src/services/CTF.ts
+++ src/services/CTF.ts
@@ -2,7 +2,15 @@ import axios from "axios";
 
 import config from "../config";
 import Logger from "../loaders/logger";
-import { InternalServerError } from "../types/httperrors";
+import { CTFModel, ICTF } from "../models/CTF";
+import { IUser, UserModel } from "../models/User";
+import { ITeam, TeamModel } from "../models/Team";
+import {
+  ForbiddenError,
+  InternalServerError,
+  NotFoundError,
+} from "../types/httperrors";
+import { verifyJWT } from "../util";
 
 export default class CTFService {
   private _hedgeDocAPI = axios.create({
@@ -32,5 +40,68 @@ export default class CTFService {
         Logger.warn("Request response code outside acceptable range.");
         throw new InternalServerError();
       });
+  }
+
+  public async createCTF(
+    jwt: string,
+    teamID: string,
+    options: any
+  ): Promise<ICTF> {
+    const decodedJWT = verifyJWT(jwt);
+
+    let user: IUser;
+    let team: ITeam;
+
+    Logger.silly("Getting user and team");
+    await Promise.all([
+      UserModel.findById(decodedJWT.id),
+      TeamModel.findById(teamID),
+    ])
+      .then(async (results) => {
+        user = await results[0].populate("teams").execPopulate();
+        team = results[1];
+      })
+      .catch((err) => {
+        Logger.verbose(err);
+        throw new InternalServerError();
+      });
+
+    if (!user) {
+      Logger.verbose("User not found");
+      throw new NotFoundError({ errorCode: "error_user_not_found" });
+    }
+
+    if (!team) {
+      Logger.verbose("Team not found");
+      throw new NotFoundError({ errorCode: "error_team_not_found" });
+    }
+
+    if (!team.members.includes(user.id) && !team.isOwner(user)) {
+      Logger.verbose("User not in team");
+      throw new ForbiddenError({
+        errorCode: "error_user_not_in_team",
+        errorMessage: "User is not in requested team",
+        details: "User cannot create CTF for a team they are not a member of",
+      });
+    }
+
+    console.log(this._hedgeDocAPI);
+
+    Logger.silly("Creating notepad");
+    const notepad = await this.createNote(options.name);
+
+    Logger.silly("Creating CTF");
+    let ctf = new CTFModel({
+      notepad: notepad.slice(1),
+      challenges: [],
+      team: team.id,
+    });
+
+    ctf = await ctf.save();
+
+    team.CTFs.push(ctf.id);
+    team.save();
+
+    return ctf;
   }
 }
