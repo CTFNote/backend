814194ce9d6ce03dac472a5b40db3805dcc5e018
S1LV3R <s1lv3r@corax.team>
2021-02-13 00:05:56 +0100

    feat: add config option for if the user is optional or not

--- src/util/middleware/user.ts
+++ src/util/middleware/user.ts
@@ -3,29 +3,32 @@ import { Request, Response, NextFunction } from "express";
 import { NotFoundError, UnauthorizedError } from "../../types/httperrors";
 import { UserModel } from "../../models/User";
 import { verifyJWT } from "../index";
+import { Middleware } from "../../types";
 
-export default async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  if (!req.headers.authorization) {
-    next(
-      new UnauthorizedError({
-        errorMessage: "Missing authorization",
-        errorCode: "error_unauthorized",
-      })
-    );
-  }
+export default (options?: { userOptional?: boolean }): Middleware => {
+  return async function (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    if (!req.headers.authorization && !options?.userOptional) {
+      next(
+        new UnauthorizedError({
+          errorMessage: "Missing authorization",
+          errorCode: "error_unauthorized",
+        })
+      );
+    }
 
-  const decodedJWT = verifyJWT(req.headers.authorization.slice(7));
+    const decodedJWT = verifyJWT(req.headers.authorization.slice(7));
 
-  const user = await UserModel.findById(decodedJWT.id).then();
+    const user = await UserModel.findById(decodedJWT.id).then();
 
-  if (!user) {
-    next(new NotFoundError({ errorCode: "error_user_not_found" }));
-  }
+    if (!user && !options?.userOptional) {
+      next(new NotFoundError({ errorCode: "error_user_not_found" }));
+    }
 
-  req.user = user;
-  next();
+    req.user = user;
+    return next();
+  };
 };
