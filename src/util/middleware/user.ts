9d6279651120ba30ce4e74a420b2dcd3cfe24c78
S1LV3R <s1lv3r@corax.team>
2021-02-16 07:50:36 +0100

    refactor: remove useless check

--- src/util/middleware/user.ts
+++ src/util/middleware/user.ts
@@ -1,11 +1,9 @@
 import { Request, Response, NextFunction } from "express";
-import { CelebrateError, Joi, Segments } from "celebrate";
 
 import { NotFoundError, UnauthorizedError } from "../../types/httperrors";
 import { UserModel } from "../../models/User";
 import { verifyJWT } from "../index";
 import { JWTData, Middleware } from "../../types";
-import { mongoDbObjectId } from "../celebrate";
 
 export default (options?: { userOptional?: boolean }): Middleware => {
   return async function (
@@ -29,20 +27,10 @@ export default (options?: { userOptional?: boolean }): Middleware => {
       return next(err);
     }
 
-    const validation = Joi.object({ userID: mongoDbObjectId }).validate({
-      userID: decodedJWT.id,
-    });
-
-    if (validation.error) {
-      const error = new CelebrateError(undefined, { celebrated: true });
-      error.details.set(Segments.PARAMS, validation.error);
-      return next(error);
-    }
-
     const user = await UserModel.findById(decodedJWT.id).then();
 
     if (!user && !options?.userOptional) {
-      next(new NotFoundError({ errorCode: "error_user_not_found" }));
+      return next(new NotFoundError({ errorCode: "error_user_not_found" }));
     }
 
     req.user = user;
