311b41d81dad632a706973a8596f1e3b46d327c0
S1LV3R <s1lv3r@corax.team>
2021-02-18 16:10:25 +0100

    refactor: disable participants from challenge
    Will be re-added later

--- src/models/Challenge.ts
+++ src/models/Challenge.ts
@@ -1,19 +1,19 @@
 import { model, Document, Schema } from "mongoose";
 
-import { IUser } from "./User";
+// import { IUser } from "./User";
 
 interface IChallenge extends Document {
   notepad: string;
   points: number;
   solved: boolean;
-  participants: Array<IUser>;
+  // participants: Array<IUser>;
 }
 
 const ChallengeSchema = new Schema<IChallenge>({
   notepad: String,
   points: Number,
   solved: Boolean,
-  participants: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  // participants: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
 
 const ChallengeModel = model<IChallenge>("Challenge", ChallengeSchema);
